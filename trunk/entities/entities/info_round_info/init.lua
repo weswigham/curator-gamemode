e2fe11848b54c8fec8dbb1b0f45d6d691d39bf3e
levybreak098 <levybreak098@b52229a6-a244-11de-923e-0b953e4d95d6>
2009-09-22 21:44:16 +0000

    heh.

---
+++ trunk/entities/entities/info_round_info/init.lua
@@ -0,0 +1,8 @@
+ENT.Type = "point"
+ENT.Base = "base_point"
+
+function ENT:Initialize() --just an empty function... this really is just a definition of a point.
+	
+end 
+
+--This entity is a bit special. But none of It's code needs to be made here.

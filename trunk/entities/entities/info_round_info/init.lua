fdf74bdc15944d13214ed47938dfe306e99699de
levybreak098 <levybreak098@b52229a6-a244-11de-923e-0b953e4d95d6>
2009-10-10 15:53:56 +0000

    Help Should now auto-open on your first time as curator and first time as thief. Start and End round triggers should now work. Updated FGD to reflect this. (Ignore the curator stuff in the event trigger, that isn't coded yet, and won't be done by the end of the competition unless i pull off something amazing)

--- trunk/entities/entities/info_round_info/init.lua
+++ trunk/entities/entities/info_round_info/init.lua
@@ -5,4 +5,12 @@ function ENT:Initialize() --just an empty function... this really is just a defi
 	
 end 
 
+function ENT:KeyValue(k,v)
+	if k == "EndRelay" then
+		self.EndRelayName = v
+	elseif k == "StartRelay" then
+		self.StartRelayName = v
+	end
+end 
+
 --This entity is a bit special. But none of It's code needs to be made here.

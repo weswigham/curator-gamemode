2580f45ce32ded333c03a6025e1ffc7b319b3d94
levybreak098 <levybreak098@b52229a6-a244-11de-923e-0b953e4d95d6>
2009-10-24 14:56:43 +0000

    curator's flashlight is now disabled, camera now has a light to show where it's looking.

--- trunk/entities/entities/curator_camera/init.lua
+++ trunk/entities/entities/curator_camera/init.lua
@@ -13,6 +13,7 @@ function ENT:Initialize()
 		phys:EnableMotion(false)
 	end
 
+	self.FlashlightTexture = "effects/flashlight001"
 	self:TemporarilyDisable()
 end
 
@@ -21,20 +22,21 @@ end
 local red = Color(255,0,0,255)
 
 function ENT:Think()
+	if not self.LastPos then self.LastPos = self:GetPos() end
+	if self.LastPos ~= self:GetPos() then --we've moved
+		self.LastPos = self:GetPos()
+		if self.flashlight then
+			self.flashlight:SetAngles( self:GetAngles() )
+			self.flashlight:SetPos(self:GetPos()+(self:GetAngles():Forward()*20))
+		end
+	end
 	if self.Active == true then
 		for k,v in ipairs(player.FindInCone(self:GetPos(),self:GetPos()+(self:GetAngles():Forward()*1000),500)) do
 			--if v ~= GAMEMODE.Curator and self:Visible(v) and self:GetPos():Distance(v:GetPos()) <= 2000 then
 			if v:IsPlayer() and v ~= GAMEMODE.Curator and self:Visible(v) then
 				v:SetNWInt("Detection",math.Clamp(v:GetNWInt("Detection")+25,0,1000))
-				debugoverlay.Cross(self:GetPos(),50,1,red)
-				debugoverlay.Line(self:GetPos(),v:GetPos(),1,red)
-				debugoverlay.Cross(v:GetPos(),50,1,red)
 			end
 		end
-		debugoverlay.Cross(self:GetPos(),50)
-		debugoverlay.Line(self:GetPos(),self:GetPos()+(self:GetAngles():Forward()*1000))
-		debugoverlay.Cross(self:GetPos()+(self:GetAngles():Forward()*1000),50)
-		debugoverlay.Sphere(self:GetPos()+(self:GetAngles():Forward()*1000),500)
 	end
 	self:NextThink(CurTime() + 0.5)
 	return true
@@ -48,10 +50,40 @@ function ENT:ReActivate()
 	self.Active = true
 	self:EmitSound("HL1/fvox/activated.wav",SNDLVL_VOICE,100)
 	print("Activated")
+	
+	local angForward = self.Entity:GetAngles()
+	
+	self.flashlight = ents.Create( "env_projectedtexture" )
+	
+		self.flashlight:SetParent( self.Entity )
+		
+		self.flashlight:SetLocalPos( Vector( 0, 0, 0 ) )
+		self.flashlight:SetLocalAngles( Angle(90,90,90) )
+		self.flashlight:SetAngles( self:GetAngles() )
+		self.flashlight:SetPos(self:GetPos()+(self:GetAngles():Forward()*20))
+		
+		self.flashlight:SetKeyValue( "enableshadows", 0 )
+		self.flashlight:SetKeyValue( "farz", 2048 )
+		self.flashlight:SetKeyValue( "nearz", 8 )
+		
+		self.flashlight:SetKeyValue( "lightfov", 45 )
+
+		self.flashlight:SetKeyValue( "lightcolor", "255 255 255" )
+		
+		
+	self.flashlight:Spawn()
+	
+	self.flashlight:Input( "SpotlightTexture", NULL, NULL, self.FlashlightTexture )
 end
 
 function ENT:DeActivate()
 	self.Active = false
+	
+	if ( self.flashlight ) then
+		SafeRemoveEntity( self.flashlight )
+		self.flashlight = nil
+		return
+	end
 end 
 
 function ENT:TemporarilyDisable(num)
